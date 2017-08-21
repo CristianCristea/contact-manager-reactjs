d393d43972d851a35f79b13297f681bcf35aeaa0
Cristian Cristea <cristiancristea.1987@gmail.com>
2017-08-21 19:44:07 +0200

    Render App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
+import NavBar from "./components/NavBar";
 import AddContact from "./components/AddContact";
 
 class App extends Component {
@@ -7,6 +8,7 @@ class App extends Component {
     return (
       <div>
         <Header title="Contact Manager" />
+        <NavBar />
         <AddContact />
       </div>
     );
